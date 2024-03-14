78ccb7aad5fa4b0e1c35598e2611ec20cdb3f67a
David Vanderheyden <64843984+David-VDH@users.noreply.github.com>
2024-03-14 19:18:30 +0200

    Initial commit

---
+++ Assets/Scripts/SelectBowlingBall.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SelectBowlingBall : MonoBehaviour
+{
+    [SerializeField] Material defaultBallMat;
+    [SerializeField] Material selectedBallMat;
+
+    private Renderer ballRenderer;
+
+    private void Awake()
+    {
+        ballRenderer = GetComponent<Renderer>();
+    }
+
+    public void OnHoverEnter()
+    {
+        ballRenderer.material = selectedBallMat;
+    }
+
+    public void OnHoverExit()
+    {
+        ballRenderer.material = defaultBallMat;
+    }
+
+}
